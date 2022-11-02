3a305f87dee63e77445527d95a47cae6b640c785
pixcelo <bodomu@gmail.com>
2022-11-03 04:11:54 +0900

    add: cryptowatch downloader template

--- downloader/historical_data_from_binance.js
+++ downloader/historical_data_from_binance.js
@@ -1,5 +1,20 @@
 import fetch from 'node-fetch';
-const url = 'https://api.github.com/users/github';
+// https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md#klinecandlestick-data
+
+// https://api.binance.com
+// GET /api/v3/klines
+
+//const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m';
+
+const startTime = new Date(2022, 11, 2, 3, 15, 30).getTime();
+//const unixTimestamp = date;
+console.log(startTime);
+
+const endTime = new Date(2022, 11, 2, 3, 20, 30).getTime();
+//const unixTimestamp = date;
+console.log(endTime);
+
+const url = `https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&startTime=${startTime}&endTime=${endTime}`;
 
 (async () => {
   try {
