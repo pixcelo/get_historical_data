dbf5b2a7587bd4e5fdd40e15bc0ce631e4850221
pixcelo <bodomu@gmail.com>
2022-11-05 12:13:46 +0900

    candles

--- downloader/historical_data_from_cryptowatch.js
+++ downloader/historical_data_from_cryptowatch.js
@@ -26,6 +26,8 @@ const helper = new Helper();
 // 若い方が昔、
 (async () => {
   try {
+      let candles = [];
+
       for (let i = 0; i < 3; i++) {
         let baseTime = {Y: 2022, M: 10, D: 1, h: 3, m: 0, s: 30}
         let after = helper.convertDateToUnixTimeStamp(baseTime, i);
@@ -34,12 +36,21 @@ const helper = new Helper();
         console.log(url);
         const response = await fetch(url);
         const json = await response.json();
-        const obj = JSON.stringify(json["result"]["60"]);
-        fs.writeFile(`./json/${after}.json`, obj, (err) => {
+        const arry = json["result"]["60"];
+        candles = candles.concat(arry);
+
+        // fs.writeFile(`./json/${after}.json`, obj, (err) => {
+        //     if (err) throw err;
+        //     console.log('success');
+        // });
+      }
+
+      console.log(candles)
+      const data = JSON.stringify(candles);
+      fs.writeFile(`./json/candles.json`, data, (err) => {
             if (err) throw err;
             console.log('success');
-        });
-      }
+      });
   } catch (error) {
       console.log(error);
   }
